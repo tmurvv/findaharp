22ab2eedcca5168fc37411ca95e45ea6a3278dc6
tmurvv <tmurv@shaw.ca>
2021-01-14 09:04:12 -0700

    switch branches

--- src/components/onlineStore/StoreProductSearchStrings.js
+++ src/components/onlineStore/StoreProductSearchStrings.js
@@ -188,7 +188,7 @@ function StoreProductSearchStrings(props) {
     return (
         <>       
         <div className='storeproductSearchOuter'>
-            <h3 className='storesearchTitle'>Searching for STRINGS? Refine your search here.<br /><img src='img/Moving_Harp.png' alt='fast truck' style={{width: '45px', transform: 'translate(-6px, 5px)'}}/><span style={{fontStyle: 'normal', fontWeight: 'bold'}}>Fast and Easy</span> string order form coming soon!!</h3>
+            <h3 className='storesearchTitle'>Searching for STRINGS? Refine your search here.<br /><img src='img/fastTruck.png' alt='fast truck' style={{width: '45px', transform: 'translate(-6px, 5px)'}}/><span style={{fontStyle: 'normal', fontWeight: 'bold'}}>Fast and Easy</span> string order form coming soon!!</h3>
             <div className='storemobileSearchLine2'>
                 <div className='storesearchLine2'>
                     <img src='./img/ribbon_gold_full.png' alt="golden background ribbon"/> 
