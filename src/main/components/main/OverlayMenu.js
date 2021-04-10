fe4dde41aab901657bdb40e730e544154f977ff5
tmurvv <tmurv@shaw.ca>
2021-04-10 08:39:03 -0600

    Overlay Menu animation draft 1

--- src/main/components/main/OverlayMenu.js
+++ src/main/components/main/OverlayMenu.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
 
@@ -5,41 +6,53 @@ import Router from 'next/router';
 import OverlayMenuCss from '../../styles/OverlayMenu.css.js';
 
 export default function OverlayMenu(props) {
+    const [ open, setOpen ] = useState(true);
     function clearOverlay(e) {
-        document.querySelector('#overlayBar').style.opacity='0';
+        
         document.querySelector('#overlayBar').style.background = 'rgb(244,244,244,0)';
+        document.querySelector('.overlayLinks').style.animation = 'myMove .8s';
         setTimeout(()=> {
-            document.querySelector('#overlayBar').style.display='none';
-        },500);
+            setOpen(false);
+        },800);
+    }
+    function restoreOverlay(e) {
+        document.querySelector('#tiles').style.animation = 'myMoveBack .8s ease-in-out';
+        setTimeout(()=> {
+            setOpen(true);
+            document.querySelector('#overlayBar').style.background = 'rgb(244,244,244,.93)';
+        }, 800);
     }
     // if (process.browser&&Router.route==='/stringform'||Router.route==='/rememberdetails'||Router.route==='/harploginsignup'||Router.route==='/userharpprofile') {
     return(
         <>
-        <div className='overlayBarOuter overlayBarOpen' id='overlayBar'>
-            <div className='overlayLinks' id='overlayLinks'>
-                {/* {props.open?
-                    <div className='closeIcon' onClick={props.handleNavOpen}>
-                        <img src='/img/clear_search.png' alt="close mobile menu icon"/>
-                    </div>:''
-                } */}
-                <Link href='/'>
-                    <a className='overlayLink thinLink' onClick={(e)=>{
-                        e.preventDefault();
-                        clearOverlay();
-                    }}><img src="/img/OverlayMenu/landing_used_nosub.png" alt="used harps" /></a>
-                </Link>
-                <Link href='/buildersshowcase'>
-                    <a className='overlayLink fatLink' onClick={(e)=>{clearOverlay(e)}}><img src="/img/OverlayMenu/landing_builders.png" alt="Builders Showcase" /></a>
-                </Link>
-                <Link href='/stringform'>
-                    <a className='overlayLink fatLink' onClick={()=>{clearOverlay(); if (Router&&Router.route!=='/onlinestore'&&document.querySelector('#spinner')) document.querySelector('#spinner').style.display='block'; props.handleNavOpen;}}><img src="/img/OverlayMenu/landing_fastneasy.png" alt="Fast and Easy String Form" /></a>
-                </Link>
-                <Link href='/onlinestore'>
-                    <a className='overlayLink thinLink' onClick={()=>{clearOverlay(); if (Router&&Router.route!=='/onlinestore'&&document.querySelector('#spinner')) document.querySelector('#spinner').style.display='block'; props.handleNavOpen;}}><img src="/img/OverlayMenu/landing_onlinestore.png" alt="Online Store" /></a>
-                </Link>        
-                
+        {open
+            ?<div className='overlayBarOuter overlayBarOpen' id='overlayBar'>
+                <div className='overlayLinks' id='overlayLinks'>
+                    {/* {props.open?
+                        <div className='closeIcon' onClick={props.handleNavOpen}>
+                            <img src='/img/clear_search.png' alt="close mobile menu icon"/>
+                        </div>:''
+                    } */}
+                    <Link href='/'>
+                        <a className='overlayLink thinLink' onClick={(e)=>{
+                            e.preventDefault();
+                            clearOverlay();
+                        }}><img src="/img/OverlayMenu/landing_used_nosub.png" alt="used harps" /></a>
+                    </Link>
+                    <Link href='/buildersshowcase'>
+                        <a className='overlayLink fatLink' onClick={(e)=>{clearOverlay(e)}}><img src="/img/OverlayMenu/landing_builders.png" alt="Builders Showcase" /></a>
+                    </Link>
+                    <Link href='/stringform'>
+                        <a className='overlayLink fatLink' onClick={()=>{clearOverlay(); if (Router&&Router.route!=='/onlinestore'&&document.querySelector('#spinner')) document.querySelector('#spinner').style.display='block'; props.handleNavOpen;}}><img src="/img/OverlayMenu/landing_fastneasy.png" alt="Fast and Easy String Form" /></a>
+                    </Link>
+                    <Link href='/onlinestore'>
+                        <a className='overlayLink thinLink' onClick={()=>{clearOverlay(); if (Router&&Router.route!=='/onlinestore'&&document.querySelector('#spinner')) document.querySelector('#spinner').style.display='block'; props.handleNavOpen;}}><img src="/img/OverlayMenu/landing_onlinestore.png" alt="Online Store" /></a>
+                    </Link>        
+                    
+                </div>
             </div>
-        </div>
+            :<img id='tiles' onClick={()=>restoreOverlay()} style={{height: '30px', zIndex: '6000', position: 'absolute', top: '198px', right: '7px'}}src='/img/OverlayMenu/tilesplaceholder.jpg' alt='opening tile menu icon' />
+        }
         <OverlayMenuCss />
        </>    
     )
--- src/main/styles/OverlayMenu.css.js
+++ src/main/styles/OverlayMenu.css.js
@@ -5,19 +5,19 @@ function OverlayMenuCss() {
         <style>{`
             #overlayBar {
                 opacity: 1;
-                background: rgb(244,244,244,.93);
+                background: rgb(244,244,244,0);
                 -webkit-transition: all 500ms ease-in;
                 -moz-transition: all 500ms ease-in;
                 -ms-transition: all 500ms ease-in;
                 -o-transition: all 500ms ease-in;
-                transition: all 500ms ease-in;
+                transition: all 2000ms ease-in;
             }
-            // .overlayBarClosed {
-            //     background: rgb(244,244,244, 0);
-            // }
-            // .overlayBarOpen {
-            //     background: rgb(244,244,244, .93);
-            // }
+            .overlayBarClosedImg {
+                document.querySelector('#overlayBar').style.transform = 'scale(.02)';
+                document.querySelector('#overlayBar').style.top = '250px';
+                document.querySelector('#overlayBar').style.right = '25px';
+                display: 'none';
+            }
             .overlayBarOuter {
                 z-index: 9999;
                 position: absolute;
@@ -27,15 +27,19 @@ function OverlayMenuCss() {
                 // background-image: url("/img/OverlayMenu/landing_background_blur_darken.png"), linear-gradient(rgb(0,0,0,.4), rgb(0,0,0,.4)); /* W3C */
                 // background-size: 500px, 600px;
             }
+           
             .overlayLinks {
                 position: absolute;
                 top: 50%;
                 right: 50%;
-                transform: translate(50%, -50%);
+                transform: translate(50%, -50%) scale(1);
                 display: flex;
-                height: 80%;
+                max-height: 80%;
                 flex-direction: column;
                 justify-content: space-around;
+                transform-origin: center;
+                transition: all 2000ms ease-in;
+                animation-iteration-count: 1;
             }        
             .overlayLink {
                 color: white;
@@ -56,7 +60,18 @@ function OverlayMenuCss() {
                 height: 100%;
                 width: 80%;
                 border-radius: 12px;
-            }        
+            }  
+            @-webkit-keyframes myMove {
+                0% { -webkit-transform: translate(50%, -50%) scale(1);  } 
+                100% { -webkit-transform: translate(800px, -300px) scale(.02); }
+            }
+            @-webkit-keyframes myMoveBack {
+                // 0% { -webkit-transform: scale(1) rotate(0deg); }
+                // 50% {-webkit-transform: scale(1.5) rotate(22.5deg); }  
+                // 100% { -webkit-transform: scale(1.5) rotate(45deg); }
+                0% { -webkit-transform: scale(1) translate(0, 0); background: rgb(244,244,244,0); }
+                100% { -webkit-transform: scale(8) translate(-40px, 20px); background: rgb(244,244,244,.93);}
+            }      
       `}</style>
     )
 }
